e32ea94d999b942eb0024ab0b9a601e6697dfd6d
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-07-08 14:44:59 +0200

    Add background image to speakers

--- speakers.js
+++ speakers.js
@@ -35,28 +35,30 @@ const firstSpeakersContainer = document.getElementsByClassName('speakers')[0];
 
 function firstSpeakers() {
   const htmlText = `<article class="speaker">
-                    <div>
-                      <img src="images/Paulo.jpg" alt="Portrait Paulo Coelho">
+                    <div class="bgimage">
+                      <img class="first" src="images/Group (1).png" alt="checked">
+                      <img class="second" src="images/Paulo.jpg" alt="Portrait Paulo Coelho">
                     </div>
 
                     <div>
                       <h3>Paulo Coelho</h3>
                       <p class="description">Brazilian lyricist and novelist</p>
                       <hr class="speakers-line">
-                      <p>Paulo Coelho is best known for his novel The Alchemist.</p>
+                      <p class="speakerDescription">Paulo Coelho is best known for his novel The Alchemist.</p>
                     </div>
                   </article>
 
                   <article class="speaker">
-                    <div>
-                      <img src="images/Lorde.jpg" alt="Portrait of Lorde">
+                    <div class="bgimage">
+                      <img class="first" src="images/Group (1).png" alt="checked">
+                      <img class="second" src="images/Lorde.jpg" alt="Portrait of Lorde">
                     </div>
 
                     <div>
                       <h3>Lorde</h3>
                       <p class="description">New Zealand Singer</p>
                         <hr class="speakers-line">
-                      <p>
+                      <p class="speakerDescription">
                         Lorde is known for employing unconventional musical styles and introspective songwriting.
                       </p>
                     </div>
@@ -67,54 +69,58 @@ function firstSpeakers() {
 
 function otherSpeakers() {
   const htmlText = `<article class="speaker otherSpeaker hideSpeaker">
-                    <div>
-                      <img src="${moreSpeaker[0].imageUrl}" alt="${moreSpeaker[0].imageAlt}">
+                    <div class="bgimage">
+                      <img class="first" src="images/Group (1).png" alt="checked">
+                      <img class="second" src="${moreSpeaker[0].imageUrl}" alt="${moreSpeaker[0].imageAlt}">
                     </div>
 
                     <div>
                       <h3>${moreSpeaker[0].name}</h3>
                       <p class="description">${moreSpeaker[0].description}</p>
                       <hr class="speakers-line">
-                      <p>${moreSpeaker[0].speakersLines}</p>
+                      <p class="speakerDescription">${moreSpeaker[0].speakersLines}</p>
                     </div>
                   </article>
                   
                   <article class="speaker otherSpeaker hideSpeaker">
-                    <div>
-                      <img src="${moreSpeaker[1].imageUrl}" alt="${moreSpeaker[1].imageAlt}">
+                    <div class="bgimage">
+                      <img class="first" src="images/Group (1).png" alt="checked">
+                      <img class="second" src="${moreSpeaker[1].imageUrl}" alt="${moreSpeaker[1].imageAlt}">
                     </div>
 
                     <div>
                       <h3>${moreSpeaker[1].name}</h3>
                       <p class="description">${moreSpeaker[1].description}</p>
                       <hr class="speakers-line">
-                      <p>${moreSpeaker[1].speakersLines}</p>
+                      <p class="speakerDescription">${moreSpeaker[1].speakersLines}</p>
                     </div>
                   </article>
 
                   <article class="speaker otherSpeaker hideSpeaker">
-                    <div>
-                      <img src="${moreSpeaker[2].imageUrl}" alt="${moreSpeaker[2].imageAlt}">
+                    <div class="bgimage">
+                      <img class="first" src="images/Group (1).png" alt="checked">
+                      <img class="second" src="${moreSpeaker[2].imageUrl}" alt="${moreSpeaker[2].imageAlt}">
                     </div>
 
                     <div>
                       <h3>${moreSpeaker[2].name}</h3>
                       <p class="description">${moreSpeaker[2].description}</p>
                       <hr class="speakers-line">
-                      <p>${moreSpeaker[2].speakersLines}</p>
+                      <p class="speakerDescription">${moreSpeaker[2].speakersLines}</p>
                     </div>
                   </article>
 
                   <article class="speaker otherSpeaker hideSpeaker">
-                    <div>
-                      <img src="${moreSpeaker[3].imageUrl}" alt="${moreSpeaker[3].imageAlt}">
+                    <div class="bgimage">
+                      <img class="first" src="images/Group (1).png" alt="checked">
+                      <img class="second" src="${moreSpeaker[3].imageUrl}" alt="${moreSpeaker[3].imageAlt}">
                     </div>
 
                     <div>
                       <h3>${moreSpeaker[3].name}</h3>
                       <p class="description">${moreSpeaker[3].description}</p>
                       <hr class="speakers-line">
-                      <p>${moreSpeaker[3].speakersLines}</p>
+                      <p class="speakerDescription">${moreSpeaker[3].speakersLines}</p>
                     </div>
                   </article>`;
 
